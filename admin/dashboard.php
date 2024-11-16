e0cb362722a628be3be436af6d9318bb317c859c
pauldungca <johnpauldungca0908@gmail.com>
2024-11-16 16:59:24 +0800

    Added the guard function!

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -2,13 +2,17 @@
 
 <?php 
 
+    include '../functions.php';
+    
+    guard();
+
     $pageTitle = "Dashboard";
     include './partials/header.php';
     include './partials/side-bar.php';
+    
+    
 
 ?>
-
-
 
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 pt-5">    
     <h1 class="h2">Dashboard</h1>        
